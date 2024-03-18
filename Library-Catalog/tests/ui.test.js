68975fe73f007e1a12582eac16b860ee96c0a934
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2024-03-18 10:44:36 +0200

    Verify "Login" button is visible

--- Library-Catalog/tests/ui.test.js
+++ Library-Catalog/tests/ui.test.js
@@ -7,4 +7,13 @@ test('Verify "All Books" link is visible', async ({page}) => {
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
     expect(isLinkVisible).toBe(true);
-})
+});
+
+test('Verify "Login" button is visible', async ({page}) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+
+    const loginButton = await page.$('#guest > a:nth-child(1)');
+    const isVisibleLoginButton = await loginButton.isVisible();
+    expect(isVisibleLoginButton).toBe(true);
+});
